b38184de3e300e6b14761bc130a7ead697b24e0c
shigemk2 <i.am.shige@gmail.com>
2016-10-31 23:56:50 +0900

    Add codes

--- src/main/scala/com/example/CompetingConsumer.scala
+++ src/main/scala/com/example/CompetingConsumer.scala
@@ -1,6 +1,26 @@
 package com.example
 
 import akka.actor._
+import akka.routing.SmallestMailboxPool
 
-object CompetingConsumerDriver {
+object CompetingConsumerDriver extends CompletableApp(100) {
+  val workItemsProvider = system.actorOf(
+    Props[WorkConsumer]
+      .withRouter(SmallestMailboxPool(nrOfInstances = 5))
+  )
+
+  for (itemCount <- 1 to 100) {
+    workItemsProvider ! WorkItem("WorkItem" + itemCount)
+  }
+}
+
+case class WorkItem(name: String)
+
+class WorkConsumer extends Actor {
+  def receive = {
+    case workItem: WorkItem =>
+      println(s"${self.path.name} for: ${workItem.name}")
+
+      CompetingConsumerDriver.completedStep()
+  }
 }
